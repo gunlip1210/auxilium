ec58eaf32a8e94d5f18a877d618fe957b83a7ccd
Park gunlip <gunlip1210@gmail.com>
2024-11-23 23:18:33 +0900

    Test API

--- app/test_page/page.js
+++ app/test_page/page.js
@@ -1,5 +1,21 @@
-export default function page() {
+export default async function page() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/nums`);
+    
+    // 응답이 JSON이 아닐 경우 에러 발생 방지
+    if (!res.ok) {
+        console.error(`Failed to fetch API: ${res.statusText}`);
+        return (
+            <div>
+                <p>Failed to load data</p>
+            </div>
+        );
+    }
+
+    const data = await res.json(); // JSON 데이터 파싱
+
     return (
-        "hello"
+        <div>
+            <p>{data.text}</p>
+        </div>
     );
 }
